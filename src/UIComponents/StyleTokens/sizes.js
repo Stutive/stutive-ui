956857949659c941eaa659ae2a87180a3dd2d5fd
AlpriElse <alprielse@gmail.com>
2019-12-30 23:04:17 -0600

    Implement useDeviceType hook

--- src/UIComponents/StyleTokens/sizes.js
+++ src/UIComponents/StyleTokens/sizes.js
@@ -1,3 +1,26 @@
+export const DEVICE_TYPE_ENUM = {
+  PHONE: 'PHONE',
+  PHABLET: 'PHABLET',
+  TABLET: 'TABLET',
+  LAPTOP: 'LAPTOP',
+  DESKTOP: 'DESKTOP'
+};
+
+/**
+ *  Device Breakpoints
+ *
+ *  Taken from Bootstrap 4:
+ *  https://getbootstrap.com/docs/4.1/layout/overview/
+ *
+ */
+export const DEVICE_BREAKPOINTS = {
+  [DEVICE_TYPE_ENUM.PHONE]: 575.98,
+  [DEVICE_TYPE_ENUM.PHABLET]: 767.98,
+  [DEVICE_TYPE_ENUM.TABLET]: 991.98,
+  [DEVICE_TYPE_ENUM.LAPTOP]: 1199.98
+};
+
+/*  Layers  */
 export const IKE_LAYER = 1;
 export const UGL_LAYER = 2;
 export const QUAD_LAYER = 1000;
