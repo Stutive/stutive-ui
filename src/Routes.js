6e044768ed904e5a1098af7a9967d371b71baade
AlpriElse <alprielse@gmail.com>
2020-06-05 00:08:29 -0500

    Add ScheduleView route

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CourseExplorerView from './containers/CourseExplorerView';
 import HomeView from './containers/HomeView';
 import GenEdView from './containers/GenEdView';
+import ScheduleView from './containers/ScheduleView';
 
 const Routes = () => {
   return (
@@ -17,6 +18,9 @@ const Routes = () => {
         </Route>
         <Route path="/general-education">
           <GenEdView />
+        </Route>
+        <Route path="/schedule">
+          <ScheduleView />
         </Route>
         <Route path="*">
           <h1>404</h1>
---
+++ src/containers/ScheduleView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ComingSoon from '../components/ComingSoon';
+import Page from '../components/Page';
+
+const ScheduleView = () => {
+  return (
+    <Page>
+      <ComingSoon />
+    </Page>
+  );
+};
+
+export default ScheduleView;
